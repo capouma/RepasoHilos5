8819b68e20126f2b8c07c65884b019238ba3ebc6
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 18:36:53 +0100

    Iniciamos el ejercicio de repaso 5. Trabajaremos el sleep en este ejercicio.

---
+++ repaso5/Repaso5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repaso5;
+
+/**
+ *
+ * @author findag
+ */
+public class Repaso5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
