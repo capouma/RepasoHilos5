a0658e5b341cd306f0afdc415d45b57230feaf33
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 18:49:32 +0100

    Mandamos como parametro el nombre  de nuestro hilo

--- repaso5/Repaso5.java
+++ repaso5/Repaso5.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Programa en java que crea dos hilos y los ejecuta.
+ * Los hilos escriben 5 veces el número de iteración del bucle y su nombre.
+ * En cada iteración, después de escribir su nombre,
+ * se bloquean durante un tiempo aleatorio de segundos y después vuelven a estar disponibles para su ejecución. 
  */
 package repaso5;
 
@@ -21,7 +22,7 @@ public class Repaso5 {
         Hilo hilo1 = new Hilo("Hilo 1");
         Hilo hilo2 = new Hilo("Hilo 2");
         
-        // Iniciamos los hilos.
+        // lanzamos los hilos.
         hilo1.start();
         hilo2.start();
         
