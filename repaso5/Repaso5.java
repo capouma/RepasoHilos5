d34b98a99c9dac660897c2e4e050fd068abc33d8
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 18:39:16 +0100

    Definimos e instanciamos nuestros dos hilos y los lanzamos.

--- repaso5/Repaso5.java
+++ repaso5/Repaso5.java
@@ -17,6 +17,14 @@ public class Repaso5 {
     public static void main(String[] args)
     {
         
+        // Definimos nuestros hilos.
+        Hilo hilo1 = new Hilo("Hilo 1");
+        Hilo hilo2 = new Hilo("Hilo 2");
+        
+        // Iniciamos los hilos.
+        hilo1.start();
+        hilo2.start();
+        
     }
     
 }
